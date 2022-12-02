8dc124bc597a31b213a391cc48a1b4f731556849
Seb-sti1 <65665540+Seb-sti1@users.noreply.github.com>
2022-12-02 08:29:03 +0100

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -1,18 +1,26 @@
-A = X = 1 # rock
-B = Y = 2 # paper
-C = Z = 3 # scissors
+A = 1 # rock
+B = 2 # paper
+C = 3 # scissors
 
-X = 0 
-Y = 3
-Z = 6
+X = 0 # lose
+Y = 3 # draw
+Z = 6 # win
 
-# do_i_win[opponent][me]
+# do_i_win[opponent - 1][me - 1]
 # -1 : lose 0 : draw 1 : win
 do_i_win = [
     [0, 1, -1],
     [-1, 0, 1],
     [1, -1, 0], 
 ]
+
+# should_play[opponent - 1][result/3]
+should_play = [
+    [C, A, B],
+    [A, B, C],
+    [B, C, A], 
+]
+
 
 def convert(letter, letterA, letterB, letterC, A, B, C):
     if letter == letterA:
@@ -33,19 +41,13 @@ Lines = file1.readlines()
 # Strips the newline character
 for line in Lines:
     opponent_letter = line.strip().split(" ")[0]
-    result = line.strip().split(" ")[1]
+    result_letter = line.strip().split(" ")[1]
     
     opponent = convert(opponent_letter, "A", "B", "C", A, B, C)
-    me = convert(my_letter, "X", "Y", "Z", X, Y, Z)
+    result = convert(result_letter, "X", "Y", "Z", X, Y, Z)
     
-    score += me
-    
-    if do_i_win[opponent - 1][me - 1] == -1:
-        score += 0
-    elif do_i_win[opponent - 1][me - 1] == 0:
-        score += 3
-    elif do_i_win[opponent - 1][me - 1] == 1:
-        score += 6
+    score += result
+    score += should_play[opponent - 1][int(result/3)] 
 
 
 print(score)
