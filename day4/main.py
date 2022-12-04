448413883b7d26d5349c0f02244337bfa5be60be
Seb-sti1 <65665540+Seb-sti1@users.noreply.github.com>
2022-12-04 06:27:28 +0100

    day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -9,18 +9,16 @@ for line in Lines:
     elve1 = line.strip().split(",")[0]
     elve2 = line.strip().split(",")[1]
     
-    elve1_range = (int(elve1.split("-")[0]), int(elve1.split("-")[1]))
-    elve2_range = (int(elve2.split("-")[0]), int(elve2.split("-")[1]))
-    
-    print(elve1_range, elve2_range)
+    elve1_range = range(int(elve1.split("-")[0]), int(elve1.split("-")[1]) + 1)
+    elve2_range = range(int(elve2.split("-")[0]), int(elve2.split("-")[1]) + 1)
     
     
-    if elve1_range[0] <= elve2_range[0] and elve2_range[1] <= elve1_range[1]:
+    inter = list(filter(lambda x: x in elve1_range, elve2_range))
+    print(elve1_range, elve2_range, inter)
+    
+    
+    if len(inter) > 0:
         print("ok")
-        score += 1
-    	
-    if elve2_range[0] <= elve1_range[0] and elve1_range[1] <= elve2_range[1] and (elve2_range[0] != elve1_range[0] or elve1_range[1] != elve2_range[1]):
-        print("ok 2")
         score += 1
     	
 
