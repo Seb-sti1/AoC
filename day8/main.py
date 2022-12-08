582e8680c83471a2673330c2374328410146c968
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-08 06:58:25 +0100

    day 8 part 2

--- day8/main.py
+++ day8/main.py
@@ -17,62 +17,48 @@ for line in Lines:
     	
     i += 1
 
-print(trees)
-
-S = 2*len(trees) + 2*(len(trees[0]) - 2)
-
 
 visible_map = [[0 for e in tree] for tree in trees]
 
+highest = 0
+
 for i in range(1, len(trees) - 1):
     for j in range(1, len(trees[0]) - 1):
-        
-        visible = True
-        for a in range(i):
+        visibility = 1
+
+        for a in range(i-1, -1, -1):
             if trees[a][j] >= trees[i][j]:
-                visible = False
                 break
+        visibility *= abs(a - i + 1) + 1
                 
-        if visible:
-            S += 1
-            visible_map[i][j] = 1
-        else:
-            visible = True
-            for a in range(i + 1, len(trees)):
-                if trees[a][j] >= trees[i][j]:
-                    visible = False
-                    break
-            
-            if visible:
-                S += 1
-                visible_map[i][j] = 1
-            else:
-                visible = True
-                for a in range(j):
-                    if trees[i][a] >= trees[i][j]:
-                        visible = False
-                        break
+        for a in range(i + 1, len(trees)):
+            if trees[a][j] >= trees[i][j]:
+                break
+        visibility *= abs(a - i - 1) + 1
+
+        for a in range(j - 1, -1, -1):
+            if trees[i][a] >= trees[i][j]:
+                break
+        visibility *= abs(a - j + 1) + 1
+
                         
-                if visible:
-                    S += 1
-                    visible_map[i][j] = 1
-                else:
-                    visible = True
-                    for a in range(j + 1, len(trees[0])):
-                        if trees[i][a] >= trees[i][j]:
-                            visible = False
-                            break
-                            
-                    if visible:
-                        S += 1
-                        visible_map[i][j] = 1
+        for a in range(j + 1, len(trees[0])):
+            if trees[i][a] >= trees[i][j]:
+                break
+        visibility *= abs(a - j - 1) + 1
+                          
+        visible_map[i][j] = visibility
+
+        highest = max(highest, visibility)
+                
+        
 
 for raw in visible_map:
     for ele in raw: 
-        print(ele, end="")
+        print(ele, end=",")
     print()
            
-print(S)   
+print(highest)
                 
                 
                 
