bd437d3f9136b348d5d82f0b7793de2ac25122b7
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-09 07:32:56 +0100

    day 9 part 2

--- day9/main.py
+++ day9/main.py
@@ -1,31 +1,66 @@
-n, m = 1000,1000
+n, m = 1000, 1000
 
 pos = [[False for i in range(m)] for j in range(n)]
 
-H_T = [500, 500, 500, 500]
+rope = [[500, 500] for i in range(10)]
 
-(x, y) = H_T[2], H_T[3] 
+(x, y) = rope[9]
 pos[x][y] = True
 
-def show_state(H_T):
 
+def show_state(rope):
     for i in range(n):
         for j in range(m):
-            
+
             S = "."
-            
-            if (i, j) == (H_T[2], H_T[3]):
-                S = "T"
-            
-            if (i, j) == (H_T[0], H_T[1]):
-                S = "H"
-            
+
+            for k in range(len(rope) - 1, -1, -1):
+
+                if i == rope[k][0] and j == rope[k][1]:
+                    if k != 0:
+                        S = str(k)
+                    else:
+                        S = "H"
             print(S, end="")
         print()
-        
     print()
 
-#show_state(H_T)
+
+def move_rope(xH, yH, xT, yT):
+    if xH - xT > 1:
+        xT += 1
+
+        if yH > yT:
+            yT += 1
+        elif yH < yT:
+            yT -= 1
+    elif xH - xT < -1:
+        xT += -1
+
+        if yH > yT:
+            yT += 1
+        elif yH < yT:
+            yT -= 1
+
+    if yH - yT > 1:
+        yT += 1
+
+        if xH > xT:
+            xT += 1
+        elif xH < xT:
+            xT -= 1
+    elif yH - yT < -1:
+        yT += - 1
+
+        if xH > xT:
+            xT += 1
+        elif xH < xT:
+            xT -= 1
+
+    return (xT, yT)
+
+
+show_state(rope)
 
 # Using readlines()
 file1 = open('input', 'r')
@@ -36,83 +71,54 @@ i = 0
 # Strips the newline character
 for line in Lines:
     move = line.strip().split(" ")
-    
+
     print(f"== {move[0]} {move[1]} ==")
 
     for i in range(int(move[1])):
-        (x, y) = H_T[0], H_T[1]
-    
+        x, y = rope[0]
+
+        # move the head
         if move[0] == "U":
-            if H_T[0] - 1 < 0:
+            if x - 1 < 0:
                 print("out")
                 break
-            H_T[0] = max(0, H_T[0] - 1)
+            rope[0][0] = max(0, x - 1)
         elif move[0] == "D":
-            if H_T[0] + 1 > n-1:
+            if x + 1 > n - 1:
                 print("out")
                 break
-            H_T[0] = min(n-1, H_T[0] + 1)
+            rope[0][0] = min(n - 1, x + 1)
         elif move[0] == "R":
-            if H_T[1] + 1 > m-1:
+            if y + 1 > m - 1:
                 print("out")
                 break
-            H_T[1] = min(m-1, H_T[1] + 1)
+            rope[0][1] = min(m - 1, y + 1)
         elif move[0] == "L":
-            if H_T[1] - 1 < 0:
+            if y - 1 < 0:
                 print("out")
                 break
-            H_T[1] = max(0, H_T[1] - 1)
-    
-        (xH, yH, xT, yT) = H_T
-        
-        if xH - xT > 1:
-            H_T[2] += 1
-            
-            if yH > yT:
-                H_T[3] += 1
-            elif yH < yT:
-                H_T[3] -= 1
-        elif xH - xT < -1:
-            H_T[2] += -1
-            
-            if yH > yT:
-                H_T[3] += 1
-            elif yH < yT:
-                H_T[3] -= 1
-       
-        if yH - yT > 1:
-            H_T[3] += 1
-            
-            if xH > xT:
-                H_T[2] += 1
-            elif xH < xT:
-                H_T[2] -= 1
-        elif yH - yT < -1:
-            H_T[3] += - 1
-            
-            if xH > xT:
-                H_T[2] += 1
-            elif xH < xT:
-                H_T[2] -= 1
+            rope[0][1] = max(0, y - 1)
 
+        for i in range(len(rope) - 1):
+            # move knots i+1 relative to i
+            (rope[i + 1][0], rope[i + 1][1]) = move_rope(rope[i][0], rope[i][1], rope[i + 1][0], rope[i + 1][1])
 
-        (x, y) = H_T[2], H_T[3] 
+        # print(rope)
+        (x, y) = rope[9]
         pos[x][y] = True
-        
-        #show_state(H_T)
-        
-        
+
+        # show_state(rope)
+
 score = 0
 
 for i in range(n):
     for j in range(m):
-         
+
         if pos[i][j]:
-            #print("#", end="")
+            # print("#", end="")
             score += 1
-        #else:
-            #print(".", end="")
-    #print()
+        # else:
+        # print(".", end="")
+    # print()
 
 print(score)
-
