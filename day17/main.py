c4690c3fcfe9e75daa9814061c12fa6c6893ab75
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-17 10:27:03 +0100

    day 17 part 2

--- day17/main.py
+++ day17/main.py
@@ -47,9 +47,6 @@ class Matrix:
         print("+-------+")
 
     def add_height(self, to_add):
-        if to_add <= 0:
-            return
-
         to_add = max(to_add, 100)
 
         for i in range(to_add):
@@ -57,12 +54,15 @@ class Matrix:
 
         self.height += to_add
 
-        N = 100000
+        N = 10_000_000
         if self.height > N:
+            print("reduce")
             self.height -= (N - 500)
+            self.highest -= (N - 500)
+            self.pos = (self.pos[0], self.pos[1] - (N - 500))
+
             self.removed_lines += (N - 500)
 
-            self.pos = (self.pos[0], self.pos[1] - (N - 500))
             self.data = self.data[(N - 500):]
 
     def spawn(self):
@@ -72,14 +72,16 @@ class Matrix:
 
         self.pos = 2, self.highest + 3
 
-        self.add_height(self.highest + 3 + piece_height - self.height)
+        to_add = self.highest + 3 + piece_height - self.height
+        if to_add > 0:
+            self.add_height(to_add)
 
     def move(self, x, y, piece):
         new_pos = self.pos[0] + x, self.pos[1] + y
 
         # check collisions
         for position in piece:
-            if 0 > position[1] + new_pos[1] or position[0] + new_pos[0] < 0 or position[0] + new_pos[0] >= 7 or\
+            if 0 > position[1] + new_pos[1] or position[0] + new_pos[0] < 0 or position[0] + new_pos[0] >= 7 or \
                     self.data[new_pos[1] + position[1]][new_pos[0] + position[0]] == 1:
                 return False
 
@@ -90,7 +92,6 @@ class Matrix:
         for position in piece:
             self.highest = max(self.highest, self.pos[1] + position[1] + 1)
             self.data[self.pos[1] + position[1]][self.pos[0] + position[0]] = 1
-
 
 
 def iterate_line(lines):
@@ -106,8 +107,11 @@ def iterate_line(lines):
     rocks = 0
     gaz_idx = 0
 
-    N = 2022
-    T = max(int(N/100), 1_000)
+    # 1_000_000_000_000
+    N = 10_000_000
+    T = max(int(N / 100), 1_000)
+
+    percent = time.time()
 
     while rocks < N:
         a = time.time_ns()
@@ -133,13 +137,15 @@ def iterate_line(lines):
         a = time.time_ns() - a
 
         if rocks % T == 0:
-            print(rocks/N*100)
+            print(rocks / N * 100)
+            print(time.time() - percent)
 
             percent_move = int(t / a * 100)
             print(f"total {t} move {percent_move}")
+            percent = time.time()
 
     print(matrix.highest + matrix.removed_lines)
 
 
-file = open('input', 'r')
+file = open('test_input', 'r')
 iterate_line(file.readlines())
