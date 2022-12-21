477a3a9c6d0404de1c18b6fb17ca78804c475248
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-21 12:15:22 +0100

    day 21 part 2

--- day19/main.py
+++ day19/main.py
@@ -30,51 +30,115 @@ class Materials:
         return n
 
     def __str__(self):
-        return f"ore {self.ore} clay {self.clay} obsidian {self.obsidian} geode {self.geode}"
+        return f"Materials : ore {self.ore} clay {self.clay} obsidian {self.obsidian} geode {self.geode}"
+
+    def __eq__(self, other):
+        return self.ore == other.ore and self.clay == other.clay and \
+            self.obsidian == other.obsidian and self.geode == other.geode
 
 
-def max_geodes(robots, materials, max_conso, blueprint, time_left=24):
-    if time_left == 0:
-        return materials.geode
+class Robots:
+    def __init__(self, ore, clay, obsidian, geode):
+        self.ore = ore
+        self.clay = clay
+        self.obsidian = obsidian
+        self.geode = geode
 
-    vls = []
+    def add(self, t, n):
+        if t == 0:
+            self.ore += n
+        elif t == 1:
+            self.clay += n
+        elif t == 2:
+            self.obsidian += n
+        elif t == 3:
+            self.geode += n
 
-    # No robot created, all mine
-    new_materials = materials.copy()
-    new_materials.ore += robots[0]
-    new_materials.clay += robots[1]
-    new_materials.obsidian += robots[2]
-    new_materials.geode += robots[3]
+    def copy(self):
+        return Robots(self.ore, self.clay, self.obsidian, self.geode)
 
-    if (new_materials.ore <= max_conso.ore and robots[0] > 0)\
-            or (new_materials.clay <= max_conso.clay and robots[1] > 0)\
-            or (new_materials.obsidian <= max_conso.obsidian and robots[2] > 0):
-        vls.append(max_geodes(robots, new_materials, max_conso, blueprint, time_left - 1))
+    def __str__(self):
+        return f"Robots : ore {self.ore} clay {self.clay} obsidian {self.obsidian} geode {self.geode}"
 
-    for i, recipe in enumerate(blueprint.robot_recipes):
-        if i == 0 and robots[0] >= max_conso.ore:
-            continue
+    def __eq__(self, other):
+        return self.ore == other.ore and self.clay == other.clay and \
+            self.obsidian == other.obsidian and self.geode == other.geode
 
-        if i == 1 and robots[1] >= max_conso.clay:
-            continue
 
-        if i == 2 and robots[2] >= max_conso.obsidian:
-            continue
+class State:
+    def __init__(self, robots, materials, blueprint, time_left):
+        self.robots = robots
+        self.materials = materials
+        self.blueprint = blueprint
+        self.time_left = time_left
 
-        if recipe[0] <= materials.ore and recipe[1] <= materials.clay and recipe[2] <= materials.obsidian:
-            new_materials_with_new_robot = new_materials.copy()
-            new_materials_with_new_robot.ore -= recipe[0]
-            new_materials_with_new_robot.clay -= recipe[1]
-            new_materials_with_new_robot.obsidian -= recipe[2]
+    def next_states(self, max_conso):
+        acceptable_states = []
 
-            new_robots = robots.copy()
-            new_robots[i] += 1
+        new_materials = self.materials.copy()
+        new_materials.ore += self.robots.ore
+        new_materials.clay += self.robots.clay
+        new_materials.obsidian += self.robots.obsidian
+        new_materials.geode += self.robots.geode
 
-            vls.append(max_geodes(new_robots, new_materials_with_new_robot, max_conso, blueprint, time_left - 1))
+        if (new_materials.ore <= max_conso.ore and self.robots.ore > 0) \
+                or (new_materials.clay <= max_conso.clay and self.robots.clay > 0) \
+                or (new_materials.obsidian <= max_conso.obsidian and self.robots.obsidian > 0) \
+                or self.robots.geode > 0:
+            acceptable_states.append(State(self.robots.copy(), new_materials, self.blueprint, self.time_left - 1))
 
-    if len(vls) == 0:
-        return materials.geode
-    return max(vls)
+        for i in range(len(self.blueprint.robot_recipes) - 1, -1, -1):
+            recipe = self.blueprint.robot_recipes[i]
+
+            if i == 0 and self.robots.ore >= max_conso.ore:
+                continue
+
+            if i == 1 and self.robots.clay >= max_conso.clay:
+                continue
+
+            if i == 2 and self.robots.obsidian >= max_conso.obsidian:
+                continue
+
+            if recipe[0] <= self.materials.ore and recipe[1] <= self.materials.clay and \
+                    recipe[2] <= self.materials.obsidian:
+                new_materials_with_new_robot = new_materials.copy()
+                new_materials_with_new_robot.ore -= recipe[0]
+                new_materials_with_new_robot.clay -= recipe[1]
+                new_materials_with_new_robot.obsidian -= recipe[2]
+
+                new_robots = self.robots.copy()
+                new_robots.add(i, 1)
+
+                acceptable_states.append(State(new_robots, new_materials_with_new_robot,
+                                               self.blueprint, self.time_left - 1))
+
+        return acceptable_states
+
+    def __str__(self):
+        return f"{str(self.robots)} ; {str(self.materials)}"
+
+    def __eq__(self, other):
+        return self.robots == other.robots and self.materials == other.materials
+
+
+def search(border, seen, max_conso, max_geodes):
+    if len(border) == 0:
+        return max_geodes
+
+    new_seen = seen + []
+    new_border = []
+
+    for state in border:
+        if state not in new_seen:
+            new_seen.append(state)
+
+            for new_state in state.next_states(max_conso):
+                if new_state not in seen and new_state not in new_border:
+                    new_border.append(new_state)
+
+    max_geodes = max([max_geodes] + [state.materials.geode for state in border])
+
+    return search(new_border, new_seen, max_conso, max_geodes)
 
 
 def iterate_line(lines):
@@ -82,8 +146,9 @@ def iterate_line(lines):
 
     for line in lines:
         if line.strip() != "":
-            b = re.search(r"^Blueprint (\d+): Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\.$",
-                          line.strip())
+            b = re.search(
+                r"^Blueprint (\d+): Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\.$",
+                line.strip())
 
             blueprints.append(Blueprint(int(b[1]),
                                         (int(b[2]), 0, 0),
@@ -101,7 +166,7 @@ def iterate_line(lines):
             max_conso.clay = max(max_conso.clay, recipe[1])
             max_conso.obsidian = max(max_conso.obsidian, recipe[2])
 
-        score += blueprint.id * max_geodes([1, 0, 0, 0], Materials(), max_conso, blueprint)
+        score += blueprint.id * search([State(Robots(1, 0, 0, 0), Materials(), blueprint, 24)], [], max_conso, 0)
 
     print(score)
 
--- day21/main.py
+++ day21/main.py
@@ -30,11 +30,14 @@ def dict_and(dict):
     return AND
 
 
-def all_necessary_monkeys(search_list, necessary_monkeys):
+def all_necessary_monkeys(search_list, necessary_monkeys, ignore_monkeys):
     # create a dict
     found = {}
     for necessary_monkey in necessary_monkeys:
-        found[necessary_monkey] = False
+        if necessary_monkey in ignore_monkeys:
+            found[necessary_monkey] = True
+        else:
+            found[necessary_monkey] = False
 
     # search for necessary monkeys
     for monkey in search_list:
@@ -47,32 +50,37 @@ def all_necessary_monkeys(search_list, necessary_monkeys):
     return dict_and(found)
 
 
-def sorted_fusion(sorted_list, to_add):
-    if len(to_add) == 0:
-        return sorted_list
-
+def sorted_fusion(sorted_list, to_add, ignore_monkeys):
     new_sorted_list = sorted_list + []
     new_to_add = []
 
     for monkey in to_add:
 
-        if all_necessary_monkeys(new_sorted_list, monkey.necessary_monkeys):
+        if all_necessary_monkeys(new_sorted_list, monkey.necessary_monkeys, ignore_monkeys):
             new_sorted_list.append(monkey)
         else:
             new_to_add.append(monkey)
 
-    return sorted_fusion(new_sorted_list, new_to_add)
+    if len(to_add) == len(new_to_add):
+        return sorted_list, new_to_add
+
+    return sorted_fusion(new_sorted_list, new_to_add, ignore_monkeys)
 
 
-def eval_monkeys(sorted_list):
-
-    values = {}
+def eval_monkeys(sorted_list, default_values=None):
+    if default_values is None:
+        values = {}
+    else:
+        values = default_values.copy()
 
     for M in sorted_list:
-        if M.value == -1:
+        if M.name not in values:
             operation = M.operation_s
 
             for necessary_monkey in M.necessary_monkeys:
+                if necessary_monkey not in values:
+                    raise Exception("No values")
+
                 operation = operation.replace(necessary_monkey, str(values[necessary_monkey]))
 
             M.value = int(eval(operation))
@@ -80,6 +88,7 @@ def eval_monkeys(sorted_list):
         values[M.name] = M.value
 
     return values
+
 
 def iterate_line(lines):
     monkeys = []
@@ -90,20 +99,55 @@ def iterate_line(lines):
         if line.strip() != "":
             name, operation_s = line.strip().split(": ")
 
+            if name == 'humn':
+                continue
+            if name == 'root':
+                operation_s = operation_s.replace("+", "-")
+
             M = Monkey(name, operation_s)
             if len(M.necessary_monkeys) == 0:
                 monkeys.append(M)
             else:
-                if all_necessary_monkeys(monkeys, M.necessary_monkeys):
+                if all_necessary_monkeys(monkeys, M.necessary_monkeys, []):
                     monkeys.append(M)
                 else:
                     cant_eval_monkeys.append(M)
 
-    monkeys_sorted = sorted_fusion(monkeys, cant_eval_monkeys)
-
+    monkeys_sorted, need_humn = sorted_fusion(monkeys, cant_eval_monkeys, [])
     values = eval_monkeys(monkeys_sorted)
 
-    print(values["root"])
+    humn_monkey = Monkey('humn', str(-1))
+    monkeys_sorted, empty = sorted_fusion([humn_monkey], need_humn, values.keys())
+
+    if len(empty) > 0:
+        raise Exception('Should be empty')
+
+    monkeys_sorted.pop(0)
+
+    S = 3_378_273_370_000
+    N = 1_000_000_000_000_000
+
+    min_root = 112609112355996
+    min_humn = 0
+
+    for humn in range(S, N, 1):
+
+        values['humn'] = humn
+
+        test_values = eval_monkeys(monkeys_sorted, values)
+
+        if humn % 10_000 == 0:
+            print(f"{(humn - S)/(N-S)*100} %")
+            print(f"humn = {humn}, root = {test_values['root']}")
+            print(f"Min : {min_root}, {min_humn}")
+
+        if test_values['root'] < min_root:
+            min_root = test_values['root']
+            min_humn = humn
+
+        if test_values['root'] == 0:
+            print(f"humn = {humn}, root = {test_values['root']}")
+            break
 
 
 file = open('input', 'r')
