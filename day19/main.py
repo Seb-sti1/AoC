f2d5528540041a0c336510635d2bd4882ac51d21
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-25 20:25:47 +0100

    day 19 part 1

--- day19/main.py
+++ day19/main.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 
 class Blueprint:
@@ -11,6 +12,8 @@ class Blueprint:
         self.robot_recipes.append(clay_robot)
         self.robot_recipes.append(obsidian_robot)
         self.robot_recipes.append(geode_robot)
+
+        self.max_conso = None
 
 
 class Materials:
@@ -35,6 +38,9 @@ class Materials:
     def __eq__(self, other):
         return self.ore == other.ore and self.clay == other.clay and \
             self.obsidian == other.obsidian and self.geode == other.geode
+
+    def to_tuple(self):
+        return self.ore, self.clay, self.obsidian, self.geode
 
 
 class Robots:
@@ -64,6 +70,18 @@ class Robots:
         return self.ore == other.ore and self.clay == other.clay and \
             self.obsidian == other.obsidian and self.geode == other.geode
 
+    def to_tuple(self):
+        return self.ore, self.clay, self.obsidian, self.geode
+
+
+def sum_prod(a, b):
+    result = 0
+
+    for i in range(len(a)):
+        result += a[i] * b[i]
+
+    return result
+
 
 class State:
     def __init__(self, robots, materials, blueprint, time_left):
@@ -72,8 +90,10 @@ class State:
         self.blueprint = blueprint
         self.time_left = time_left
 
-    def next_states(self, max_conso):
+    def next_states(self):
         acceptable_states = []
+
+        max_conso = self.blueprint.max_conso
 
         new_materials = self.materials.copy()
         new_materials.ore += self.robots.ore
@@ -81,15 +101,13 @@ class State:
         new_materials.obsidian += self.robots.obsidian
         new_materials.geode += self.robots.geode
 
-        if (new_materials.ore <= max_conso.ore and self.robots.ore > 0) \
-                or (new_materials.clay <= max_conso.clay and self.robots.clay > 0) \
-                or (new_materials.obsidian <= max_conso.obsidian and self.robots.obsidian > 0) \
+        if (self.materials.ore <= max_conso.ore and self.robots.ore > 0) \
+                or (self.materials.clay <= max_conso.clay and self.robots.clay > 0) \
+                or (self.materials.obsidian <= max_conso.obsidian and self.robots.obsidian > 0) \
                 or self.robots.geode > 0:
             acceptable_states.append(State(self.robots.copy(), new_materials, self.blueprint, self.time_left - 1))
 
-        for i in range(len(self.blueprint.robot_recipes) - 1, -1, -1):
-            recipe = self.blueprint.robot_recipes[i]
-
+        for i, recipe in enumerate(self.blueprint.robot_recipes):
             if i == 0 and self.robots.ore >= max_conso.ore:
                 continue
 
@@ -112,33 +130,40 @@ class State:
                 acceptable_states.append(State(new_robots, new_materials_with_new_robot,
                                                self.blueprint, self.time_left - 1))
 
-        return acceptable_states
+        return acceptable_states[::-1]
 
-    def __str__(self):
-        return f"{str(self.robots)} ; {str(self.materials)}"
+    def theoretical_max(self):
+        max_geodes = self.materials.geode
 
-    def __eq__(self, other):
-        return self.robots == other.robots and self.materials == other.materials
+        j = 0
+        for i in range(self.time_left, -1, -1):
+            max_geodes += (self.robots.geode + j) * i
+            j += 1
 
-
-def search(border, seen, max_conso, max_geodes):
-    if len(border) == 0:
         return max_geodes
 
-    new_seen = seen + []
-    new_border = []
+    def __str__(self):
+        return f"{str(self.robots)} ; {str(self.materials)} ; {self.time_left}"
 
-    for state in border:
-        if state not in new_seen:
-            new_seen.append(state)
+    def __eq__(self, other):
+        if isinstance(other, tuple):
+            return self.to_tuple() == other
+        return self.robots == other.robots and self.materials == other.materials
 
-            for new_state in state.next_states(max_conso):
-                if new_state not in seen and new_state not in new_border:
-                    new_border.append(new_state)
+    def to_tuple(self):
+        return self.robots.to_tuple() + self.materials.to_tuple() + (self.time_left,)
 
-    max_geodes = max([max_geodes] + [state.materials.geode for state in border])
 
-    return search(new_border, new_seen, max_conso, max_geodes)
+def dfs(state, max_geodes):
+    if state.time_left == 0:
+        return max(state.materials.geode, max_geodes)
+
+    for new_state in state.next_states():
+
+        if max_geodes < 0 or new_state.theoretical_max() >= max_geodes:
+            max_geodes = max(dfs(new_state, max_geodes), max_geodes)
+
+    return max_geodes
 
 
 def iterate_line(lines):
@@ -166,10 +191,17 @@ def iterate_line(lines):
             max_conso.clay = max(max_conso.clay, recipe[1])
             max_conso.obsidian = max(max_conso.obsidian, recipe[2])
 
-        score += blueprint.id * search([State(Robots(1, 0, 0, 0), Materials(), blueprint, 24)], [], max_conso, 0)
+        blueprint.max_conso = max_conso
+
+        max_geodes = dfs(State(Robots(1, 0, 0, 0), Materials(), blueprint, 24), 0)
+        print(f"Max geodes for blueprint {blueprint.id} is {max_geodes}")
+
+        score += blueprint.id * max_geodes
 
     print(score)
 
 
-file = open('test_input', 'r')
+file = open('input', 'r')
+t = time.time()
 iterate_line(file.readlines())
+print(f"it took {time.time() - t}")
